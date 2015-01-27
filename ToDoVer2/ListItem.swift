4f7ed6ff39308f974d9ecafe467a4bdf2bc3b610
Stuart Robinson <stuart.robinson@digitaslbi.com>
2015-01-27 11:11:31 +0000

    Moving files

---
+++ ToDoVer2/ListItem.swift
@@ -0,0 +1,15 @@
+//
+//  ListItem.swift
+//  ToDoVer2
+//
+//  Created by robinsos on 27/01/2015.
+//  Copyright (c) 2015 robinsos. All rights reserved.
+//
+
+import Foundation
+
+struct ListItem {
+    var title : String
+    var timestamp : NSDate
+    var listID : String
+}
